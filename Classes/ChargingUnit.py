82694d11fc4db281af18406b0c33c6f17229f040
artadsm <arturanacleto2@gmail.com>
2025-02-01 19:20:52 +0100

    feature: Classes ElectricVehicle and ChargingUnit added

---
+++ Classes/ChargingUnit.py
@@ -0,0 +1,5 @@
+class ChargingUnit():
+    def __init__(self,maxPower=22,connectionStatus = False):
+        self.maxPower = maxPower 
+        self.connectionStatus = connectionStatus
+        
---
+++ Classes/ElectricVehicle.py
@@ -0,0 +1,6 @@
+class EletricVehicle():
+    def __init__(self,maxPower = 11,batteryCapacity = 50,stateOfCharge = 10,connectionStatus = False):
+        self.maxPower = maxPower
+        self.batteryCapacity = batteryCapacity
+        self.stateOfCharge = stateOfCharge
+        self.connectionStatus = connectionStatus
